35b5bb25e5bdf5a2a0312a19c9c46625666831b4
Scott Morales <morales.scott@gmail.com>
2025-07-30 14:11:51 -0500

    adds original content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>Scott Morales</h1>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start ">
+        <div className="prose">
+          <h1>Scott Morales</h1>
+          <p>Software developer by day, entrepreneur by night. With a CS degree, co owner of a web dev & marketing agency, and a local service startup under my belt - I’m all about building cool stuff that works.</p>
+        </div>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <Link
