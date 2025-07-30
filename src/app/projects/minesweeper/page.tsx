867e8302847fa5d7c6e4e0a1c9fdef6704e5ae18
Scott Morales <morales.scott@gmail.com>
2025-07-30 09:25:05 -0500

    adds page

---
+++ src/app/projects/minesweeper/page.tsx
@@ -0,0 +1,7 @@
+export default function Minesweeper() {
+    return (
+        <div className="prose">
+            <h1>Not here yet..</h1>
+        </div>
+    );
+}
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+import Minesweeper from "./minesweeper/page";
+
 export default function Projects() {
-  return (<div className="prose">
-    <h1>
-      Projects
-    </h1>
-    <p>Coming soon!</p>
-  </div>);
+  return (
+    <div className="prose">
+      <h1>
+        Projects
+      </h1>
+      <Link href="/projects/minesweeper">Play my JS Minesweeper clone!</Link >
+    </div>);
 }
